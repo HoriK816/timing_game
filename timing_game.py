95e064e71328305faf813c360b42be8def1136e8
HoriK816 <horishi.root2@gmail.com>
2023-02-23 14:30:53 +0900

    this is the finished product of a timing game

--- timing_game.py
+++ timing_game.py
@@ -11,6 +11,12 @@ pygame.mixer.init(44100)
 pygame.mixer.music.load("./sounds/music/texture1.mp3")
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(loops=-1)
+player_attack = False 
+player_attack_cycle = 0.0
+
+# images
+player_char_img = pygame.image.load("./img/me.png")
+enemy_char_img = pygame.image.load("./img/enemy.png")
 
 # font configure
 judge_font = pygame.font.SysFont(None, 24)
@@ -24,6 +30,7 @@ judge_text = {
 damage_font  = pygame.font.SysFont(None, 24)
 system_font = pygame.font.SysFont(None,24)
 
+op_text = system_font.render("Timing Game", True,(255,255,255))
 clear_text = system_font.render("Game Clear!",True,(255,255,255))
 
 # color 
@@ -33,24 +40,20 @@ BLUE = (0,0,255)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-# the position of the timing bar
+# timing bar 
 timing_bar_x = 100
 timing_bar_y = 300 
 timing_bar_width = 800
 timing_bar_height = 100
-
 bar_pos = 0
 bar_width = 3
+hit_pos = 0 
 counter_flag = True
 shot_flag = False
-hit_pos = 0 
+s_gap = 10
+a_gap = 30
+b_gap = 70
 
-# load images
-player_char_img = pygame.image.load("./img/me.png")
-enemy_char_img = pygame.image.load("./img/enemy.png")
-
-player_attack = False 
-player_attack_cycle = 0.0
 
 enemy_attack = False
 enemy_attack_cycle = 0.0
@@ -58,11 +61,12 @@ enemy_attack_cycle = 0.0
 # hp
 enemy_hp = 100
 
+process = 0
+attack_flag = False
+shot_flag = False
 
-# flag
-clear_flag = False
 
-def draw_player_character():
+def draw_player():
     global player_attack, player_attack_cycle 
 
     screen.blit(player_char_img,(700-(int(player_attack_cycle)*20),100))
@@ -73,7 +77,7 @@ def draw_player_character():
         player_attack = False
         player_attack_cycle = 0
 
-def draw_enemy_character():
+def draw_enemy():
     global enemy_attack, enemy_attack_cycle
 
     screen.blit(enemy_char_img,(200+(int(enemy_attack_cycle)*20),100)) 
@@ -84,42 +88,7 @@ def draw_enemy_character():
         enemy_attack = False
         enemy_attack_cycle = 0
 
-def attack_enemy_calc(score):
-    global enemy_hp
-    global clear_flag
-
-    if score == 's':
-        damage = random.randint(25,35)
-    elif score == 'a':
-        damage = random.randint(15,25)
-    elif score == 'b':
-        damage = random.randint(5,15)
-    else:
-        damage = random.randint(1,5)
-
-    enemy_hp -= damage 
-    if enemy_hp < 0:
-        clear_flag = True
-
-while not(clear_flag):
-
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                hit_pos = bar_pos 
-                shot_flag = True
-                player_attack = True
-                already_attack = False
-
-        elif event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # enemy
-    draw_enemy_character()
-
-    # player
-    draw_player_character()
+def draw_timing_bar():
 
     # background of the timing bar
     pygame.draw.rect(screen, WHITE, 
@@ -132,13 +101,49 @@ while not(clear_flag):
             (bar_pos + timing_bar_x, timing_bar_y + timing_bar_height),
             width=bar_width)
 
+
+def damage_calc(score):
+    global enemy_hp, attack_flag, process
+    
+    if not(attack_flag):
+
+        if score == 's':
+            damage = random.randint(25,35)
+        elif score == 'a':
+            damage = random.randint(15,25)
+        elif score == 'b':
+            damage = random.randint(5,15)
+        else:
+            damage = random.randint(1,5)
+
+        enemy_hp -= damage 
+        if enemy_hp < 0:
+            process += 1
+
+    attack_flag = True
+
+
+def calc_bar_position():
+    global counter_flag, timing_bar_width, bar_width, bar_pos
+    global attack_flag, shot_flag 
+
+    if counter_flag:
+        if bar_pos < timing_bar_width - bar_width:
+            bar_pos += 1
+        else:
+            bar_pos = 0
+            attack_flag = False
+            shot_flag = False
+        counter_flag = False
+    else:
+        counter_flag = True
+
+def draw_lines():
+    global s_gap, a_gap, b_gap 
+    global timing_bar_width, timing_bar_x     
+
     center_pos = timing_bar_width/2 + timing_bar_x 
-    
-    # s line
-    s_gap = 10
-    a_gap = 30
-    b_gap = 70
-    
+
     a_line_len = timing_bar_height - 60 
     b_line_len = timing_bar_height - 25 
 
@@ -174,56 +179,92 @@ while not(clear_flag):
             (center_pos + b_gap, timing_bar_y + timing_bar_height - b_line_len/2),
             width=bar_width)
 
-    if shot_flag:
-        pygame.draw.line(screen, GREEN,
-                (hit_pos + timing_bar_x, timing_bar_y),
-                (hit_pos + timing_bar_x, timing_bar_y + timing_bar_height),
-                width=bar_width)
 
-        distance = abs(hit_pos-(timing_bar_width/2))
-        if distance < s_gap:
-            screen.blit(judge_text["s"],(hit_pos + timing_bar_x, timing_bar_y + 140))
-            score = "s"
-        elif distance < a_gap:
-            screen.blit(judge_text["a"],(hit_pos + timing_bar_x, timing_bar_y + 140))
-            score = "a"
-        elif distance < b_gap:
-            screen.blit(judge_text["b"],(hit_pos + timing_bar_x, timing_bar_y + 140))
-            score = "b"
-        else:
-            screen.blit(judge_text["c"],(hit_pos + timing_bar_x, timing_bar_y + 140))
-            score = "c"
+def hit_check(hit_pos): 
+    global s_gap, a_gap, b_gap
 
-        if not(already_attack):
-            attack_enemy_calc(score)
-            already_attack = True
+    distance = abs(hit_pos-(timing_bar_width/2))
 
-
-    damage_text = damage_font.render(str(enemy_hp),True,(255,0,0))
-    screen.blit(damage_text,(200,10))
-
-    pygame.display.update()
-    screen.fill(BLACK)
-
-
-    if counter_flag:
-        if bar_pos < timing_bar_width - bar_width:
-            bar_pos += 1
-        else:
-            bar_pos = 0
-        counter_flag = False
+    if distance < s_gap:
+        screen.blit(judge_text["s"],(hit_pos + timing_bar_x, timing_bar_y + 140))
+        score = "s"
+    elif distance < a_gap:
+        screen.blit(judge_text["a"],(hit_pos + timing_bar_x, timing_bar_y + 140))
+        score = "a"
+    elif distance < b_gap:
+        screen.blit(judge_text["b"],(hit_pos + timing_bar_x, timing_bar_y + 140))
+        score = "b"
     else:
-        counter_flag = True
+        screen.blit(judge_text["c"],(hit_pos + timing_bar_x, timing_bar_y + 140))
+        score = "c"
 
+    return score
+
+op_timer = 1000
 
 while True:
 
+    op_timer -= 1
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_SPACE) and not (attack_flag):
+                hit_pos = bar_pos 
+                shot_flag = True
+                player_attack = True
+                attack_flag = False
+
+        elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    screen.blit(clear_text,(450,250))
-    pygame.display.update()
-    screen.fill(BLACK)
+
+    if process == 0:
+        
+        screen.blit(op_text,(450,250))
+        pygame.display.update()
+        screen.fill(BLACK)
+
+        if op_timer == 0:
+            process += 1
+
+    if process == 1:
+
+
+        draw_enemy()
+        draw_player()
+        draw_timing_bar()
+        draw_lines()
+
+
+        calc_bar_position()
+
+        if shot_flag:
+            pygame.draw.line(screen, GREEN,
+                    (hit_pos + timing_bar_x, timing_bar_y),
+                    (hit_pos + timing_bar_x, timing_bar_y + timing_bar_height),
+                    width=bar_width)
+            
+            score = hit_check(hit_pos)
+
+            damage_calc(score)
+
+
+        damage_text = damage_font.render(str(enemy_hp),True,(255,0,0))
+        screen.blit(damage_text,(200,10))
+
+        pygame.display.update()
+        screen.fill(BLACK)
+
+
+
+    elif process == 2:
+
+        screen.blit(clear_text,(450,250))
+        pygame.display.update()
+        screen.fill(BLACK)
+
+
+
+
 
